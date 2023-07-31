ff6bb88bb1cf21eb774ba2aeb68d5dbb7fc2765a
mostafa-soliman <m.fadaly52@gmail.com>
2023-08-01 01:30:03 +0200

    add to ts func. generation img to html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AbuWessam';
+  images: string[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.images = this.generateGalleryLinks(57).concat(
+      this.generateGalleryHLinks(40)
+    );
+  }
+
+  generateGalleryLinks(count: number): string[] {
+    return Array.from(Array(count).keys()).map(
+      (i) => `../assets/img/gallery(${i + 1}).jpeg`
+    );
+  }
+
+  generateGalleryHLinks(count: number): string[] {
+    return Array.from(Array(count).keys()).map(
+      (i) => `../assets/img/galleryH(${i + 1}).jpeg`
+    );
+  }
 }
